60fd74de190fa04b85f63a3efbf67cf107109f24
Pedrobrgss <pedrobrgss1@gmail.com>
2025-04-01 08:05:00 -0300

    feat(service) = Criação do método criarPratoPrincipal para criação no CRUD

--- restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalService.java
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalService.java
@@ -2,6 +2,7 @@ package br.com.cursoSpring.restaurante.pratoPrincipal;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +13,15 @@ public class PratoPrincipalService {
     private final PratoPrincipalRepository pratoPrincipalRepository;
     private final ModelMapper modelMapper;
 
+    @PostMapping // Mapeia a requisição HTTP POST para este método
+    public PratoPrincipalDTO criarPratoPrincipal(PratoPrincipalDTO dto) {
+        // Converte o DTO recebido para a entidade PratoPrincipal
+        PratoPrincipal pratoPrincipal = modelMapper.map(dto, PratoPrincipal.class);
+
+        // Salva a entidade no banco de dados
+        pratoPrincipalRepository.save(pratoPrincipal);
+
+        // Converte a entidade salva de volta para DTO e retorna
+        return modelMapper.map(pratoPrincipal, PratoPrincipalDTO.class);
+    }
 }
