8eff8b3a87d5c09ab71714346b0a0a3ec9bf36d2
Pedrobrgss <pedrobrgss1@gmail.com>
2025-04-03 09:09:59 -0300

    feat(service): adiciona o método buscarTodos para listar os pratos principais

--- restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalService.java
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalService.java
@@ -1,16 +1,24 @@
 package br.com.cursoSpring.restaurante.pratoPrincipal;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
+import br.com.cursoSpring.restaurante.config.Configurations;
 import lombok.RequiredArgsConstructor;
 
 @Service
 //cria o construtor dos atributos que são requisitados
 @RequiredArgsConstructor
 public class PratoPrincipalService {
+
+    private final Configurations configurations;
     private final PratoPrincipalRepository pratoPrincipalRepository;
     private final ModelMapper modelMapper;
+
+    PratoPrincipalService(Configurations configurations) {
+        this.configurations = configurations;
+    }
 
     public PratoPrincipalDTO criarPratoPrincipal(PratoPrincipalDTO dto) {
         // Converte o DTO recebido para a entidade PratoPrincipal
@@ -21,5 +29,9 @@ public class PratoPrincipalService {
 
         // Converte a entidade salva de volta para DTO e retorna
         return modelMapper.map(pratoPrincipal, PratoPrincipalDTO.class);
+    }
+
+    public List<PratoPrincipalDTO> buscarTodos(){
+        return pratoPrincipalRepository.findAll().stream().map(p-> modelMapper.map(p,PratoPrincipalDTO.class)).collect(Collectors.toList());
     }
 }
