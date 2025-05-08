d95c6dbf01b9c576e63da6ac965ed6b487d92682
Pedrobrgss <pedrobrgss1@gmail.com>
2025-05-08 09:38:42 -0300

    feat:Padronizando a API no estilo Response Entity

--- restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalController.java
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/pratoPrincipal/PratoPrincipalController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+
+import java.net.URI;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,29 +30,37 @@ public class PratoPrincipalController {
     private final PratoPrincipalService pratoPrincipalService; // Injeta o serviço responsável pela lógica de negócios
 
     @PostMapping // Mapeia a requisição HTTP POST para este método
-    public void cadastrar(@RequestBody @Valid PratoPrincipalDTO dto) {
-        pratoPrincipalService.criarPratoPrincipal(dto); // Chama o serviço para cadastrar o prato principal
+    public ResponseEntity<PratoPrincipalDTO> cadastrar(@RequestBody @Valid PratoPrincipalDTO dto, UriComponentsBuilder uriBuilder) {
+        PratoPrincipalDTO pratoPrincipalDTO = pratoPrincipalService.criarPratoPrincipal(dto); 
+
+        URI endereco = uriBuilder.path("/pratosprincipais/{id}").buildAndExpand(pratoPrincipalDTO.getId()).toUri();
+
+        return ResponseEntity.created(endereco).body(pratoPrincipalDTO);
     }
 
     @GetMapping
-    public Page<PratoPrincipalDTO> buscarTodos(@PageableDefault(size = 10) Pageable paginacao) {
-        return pratoPrincipalService.buscarTodos(paginacao);
+    public ResponseEntity<Page<PratoPrincipalDTO>> buscarTodos(@PageableDefault(size = 10) Pageable paginacao) {
+        Page<PratoPrincipalDTO> page = pratoPrincipalService.buscarTodos(paginacao);
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public PratoPrincipalDTO buscarPorID(@PathVariable @NotNull long id) {
+    public ResponseEntity<PratoPrincipalDTO> buscarPorID(@PathVariable @NotNull long id) {
 
-        return pratoPrincipalService.buscarPorID(id);
+        PratoPrincipalDTO pratoPrincipalDTO = pratoPrincipalService.buscarPorID(id);
+
+        return ResponseEntity.ok(pratoPrincipalDTO);
     }
     
     @PutMapping("{id}")
-        public PratoPrincipalDTO atualizar(@PathVariable @NotNull Long id, @RequestBody @Valid PratoPrincipalDTO dto ){
+        public ResponseEntity<PratoPrincipalDTO> atualizar(@PathVariable @NotNull Long id, @RequestBody @Valid PratoPrincipalDTO dto ){
             PratoPrincipalDTO pratoAtualizado = pratoPrincipalService.atualizarPrato(id, dto);
-            return pratoAtualizado;
+            return ResponseEntity.ok(pratoAtualizado);
         }
 
     @DeleteMapping("{id}")
-        public void deletar(@PathVariable @NotNull Long id){
+        public ResponseEntity<PratoPrincipalDTO> deletar(@PathVariable @NotNull Long id){
             pratoPrincipalService.deletar(id);
+            return ResponseEntity.noContent().build();
         }
 }
