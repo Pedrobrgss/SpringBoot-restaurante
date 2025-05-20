297fa354b001b92eaecc1c8c754819f6e936a84d
Pedrobrgss <pedrobrgss1@gmail.com>
2025-05-20 10:04:06 -0300

    feat: adição da entidade usuario para login

---
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package br.com.cursoSpring.restaurante.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Usuario {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String senha;
+}
