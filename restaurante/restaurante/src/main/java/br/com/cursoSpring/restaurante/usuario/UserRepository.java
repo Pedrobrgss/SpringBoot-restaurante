dd37c9693b13ceca8741465c5031fc67ea9f1fa4
Pedrobrgss <pedrobrgss1@gmail.com>
2025-05-21 08:10:07 -0300

    feat: Adição do UserRepository e UserService para implementação do usuário

---
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/usuario/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.cursoSpring.restaurante.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByUserName(String username);
+}
--- restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/usuario/Usuario.java
+++ restaurante/restaurante/src/main/java/br/com/cursoSpring/restaurante/usuario/Usuario.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuarios")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
